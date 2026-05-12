9d4aa3fa1afcd752aac0998bd9d0a334e01a3efd
EslamKamel89 <elsamkamelforex@gmail.com>
2026-05-12 10:55:36 +0300

    chore(release): bump app version to 1.0.7 and update dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         // versionCode = flutter.versionCode
         // versionName = flutter.versionName
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
     }
 
     signingConfigs {
